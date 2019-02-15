1bde15a93183b185929fcb9a7f2fc4eab502b568
Yash Trikannad <yash.trikannad@gmail.com>
2019-02-14 20:45:40 -0500

    Started working on UKF

--- unscented_kalman_filter/estimate_rot.py
+++ unscented_kalman_filter/estimate_rot.py
@@ -1,31 +1,33 @@
 import scipy.io as sio
 import numpy as np
 from unscented_kalman_filter.ukf import ukf
-import helper_functions
+
 
 def run(acc, vel, ts):
 
-	# Initialize P_k-1 and Q
-	P = np.eye(6)
-	Q = np.eye(6)
+    # Initialize P_k-1 and Q
+    P = np.eye(6)
+    Q = np.eye(6)
 
-	q = [1/2, 1/2, 1/2, 1/2]
-	w = [1, 1, 1]
-	x = np.transpose(np.concatenate([q, w]))
-	# Do transformation of sigma point
-	u = ukf()
+    q = [1/2, 1/2, 1/2, 1/2]
+    w = [1, 1, 1]
+    x = np.transpose(np.concatenate([q, w]))
+    # Do transformation of sigma point
+    delta_time = 1e-9 * (ts[1]-ts[0])
+    u = ukf()
+    Wi = u.sigma_w_calculation(P, Q)
+    Sigma_Points = u.Xi_calculation(Wi, x)
+    u.Xi_propagation(Sigma_Points, delta_time)
 
-	Wi = u.sigma_w_calculation(P, Q)
-	u.Xi_calculation(Wi, x)
 
 def estimate_rot(data_num):
-	# your code goes here
-	imu_data = sio.loadmat("imu/imuRaw%d.mat" % data_num)
-	imu_measurements = np.asarray(imu_data['vals'])
-	linear_acc = imu_measurements[0:3, :]
-	angular_vel = imu_measurements[3:, :]
-	imu_ts = np.asarray(imu_data['ts'])
-	run(linear_acc, angular_vel, imu_ts)
+    # your code goes here
+    imu_data = sio.loadmat("imu/imuRaw%d.mat" % data_num)
+    imu_measurements = np.asarray(imu_data['vals'])
+    linear_acc = imu_measurements[0:3, :]
+    angular_vel = imu_measurements[3:, :]
+    imu_ts = np.asarray(imu_data['ts'])
+    run(linear_acc, angular_vel, imu_ts)
 
 
 estimate_rot(1)
--- unscented_kalman_filter/ukf.py
+++ unscented_kalman_filter/ukf.py
@@ -10,7 +10,7 @@ class quaternion_class:
 
     def convert3dto4d(self, w):
         angle = np.linalg.norm(w, axis=0)
-        axis = w/(angle+0.0001)
+        axis = w/(angle+0.00000001)
         quaternion = np.vstack((np.cos(angle / 2), axis*np.sin(angle / 2)))
         return quaternion
 
@@ -38,10 +38,20 @@ class ukf(quaternion_class):
     def Xi_calculation(self, root, statek_1):
         q = self.convert3dto4d(root[0:3, :])
         q_k = self.quaternion_multiplication(statek_1[0:4], q)
-        statek_1 = np.transpose(np.repeat(np.reshape(statek_1, (np.size(statek_1[0]), -1)), 12, axis=0))
-        w_k = root[3:, ]+statek_1[4:, ]
+        statek_1_repeated = np.transpose(np.repeat(np.reshape(statek_1, (np.size(statek_1[0]), -1)), 12, axis=0))
+        w_k = root[3:, ]+statek_1_repeated[4:, ]
         x_k = np.vstack((q_k, w_k))
+        x_k = np.hstack((np.expand_dims(statek_1, axis=1), x_k))
         return x_k
+
+    def Xi_propagation(self, x_k, delta_t):
+        previous_angular = x_k[-3:]
+        angle = np.linalg.norm(previous_angular, axis=0)*delta_t
+        axis = previous_angular/(angle+0.00000001)
+        quat_delta = np.vstack((np.cos(angle / 2), axis * np.sin(angle / 2)))
+        q = self.quaternion_multiplication(x_k[:4, :], quat_delta)
+        x_predicted = np.vstack((q, previous_angular))
+        return x_predicted
 
 
 
