02d740f034efa8ee12049820186b6aa213156ae0
Yash Trikannad <yash.trikannad@gmail.com>
2019-02-17 18:33:14 -0500

    Debugging left

--- unscented_kalman_filter/estimate_rot.py
+++ unscented_kalman_filter/estimate_rot.py
@@ -8,17 +8,24 @@ def run(acc, vel, ts):
     # Initialize P_k-1 and Q
     P = np.eye(6)
     Q = np.eye(6)
-
+    R = np.eye(3)
     q = [1/2, 1/2, 1/2, 1/2]
     w = [1, 1, 1]
     x = np.transpose(np.concatenate([q, w]))
     # Do transformation of sigma point
-    delta_time = 1e-9 * (ts[1]-ts[0])
+    delta_time = (ts[0, 1]-ts[0, 0])
     u = ukf()
     Wi = u.sigma_w_calculation(P, Q)
     Sigma_Points = u.Xi_calculation(Wi, x)
-    u.Xi_propagation(Sigma_Points, delta_time)
+    Yi = u.Xi_propagation(Sigma_Points, delta_time)
+    xk_, ei = u.apriori_mean_estimate(Yi, x)
+    Pk_, Wi6d_ = u.covariance_Yi(x, Yi, ei)
 
+    # Measurement Model
+    zk_, Z_i, vk = u.getMeasurements(Yi, "gyro")
+    Pvv = u.getMeasurementCovariance(zk_, Z_i, R)
+    Pxz = u.CrossCorrelation(Wi6d_, Z_i, zk_)
+    u.KalmanGain(Pxz, Pvv, xk_,vk, Pk_ )
 
 def estimate_rot(data_num):
     # your code goes here
--- unscented_kalman_filter/ukf.py
+++ unscented_kalman_filter/ukf.py
@@ -1,17 +1,24 @@
 import numpy as np
 
-
 class quaternion_class:
 
     def convert4dto3d(self, q):
-        theta = 2 * np.arccos(q[0])
-        rot_vector = (theta / np.sin(theta / 2)) * [q[1:]]
+        q = np.clip(q, -1, 1)
+        theta = np.expand_dims(2*np.arccos(q[0]), axis=0)
+        theta = np.repeat(theta, 3, axis=0)
+        rot_vector = (theta / (np.sin((theta+0.00001) / 2))) * q[1:, :]
         return rot_vector
 
     def convert3dto4d(self, w):
         angle = np.linalg.norm(w, axis=0)
         axis = w/(angle+0.00000001)
         quaternion = np.vstack((np.cos(angle / 2), axis*np.sin(angle / 2)))
+        return quaternion
+
+    def convert3dto4done(self, w):
+        angle = np.linalg.norm(w, axis=0)
+        axis = w/(angle+0.00000001)
+        quaternion = np.hstack((np.array(np.cos(angle / 2)), axis*np.sin(angle / 2)))
         return quaternion
 
     def quaternion_multiplication(self, q0_vec, q1_vec):
@@ -22,8 +29,13 @@ class quaternion_class:
         y = np.array([w0 * y1 + y0 * w1 + z0 * x1 - x0 * z1])
         z = np.array([w0 * z1 + z0 * w1 + x0 * y1 - y0 * x1])
         result = np.vstack((w, x, y, z))
+        result = result/(np.linalg.norm(result, axis=0))
         return result
 
+    def quaternion_inverse(self, q):
+        q_inverse = np.array([q[0], -q[1], -q[2], -q[3]])
+        mag = np.linalg.norm(q)
+        return q_inverse/mag
 
 class ukf(quaternion_class):
 
@@ -41,7 +53,6 @@ class ukf(quaternion_class):
         statek_1_repeated = np.transpose(np.repeat(np.reshape(statek_1, (np.size(statek_1[0]), -1)), 12, axis=0))
         w_k = root[3:, ]+statek_1_repeated[4:, ]
         x_k = np.vstack((q_k, w_k))
-        x_k = np.hstack((np.expand_dims(statek_1, axis=1), x_k))
         return x_k
 
     def Xi_propagation(self, x_k, delta_t):
@@ -53,6 +64,67 @@ class ukf(quaternion_class):
         x_predicted = np.vstack((q, previous_angular))
         return x_predicted
 
+    def apriori_mean_estimate(self, Yi, xk_1):
+        omega_i = Yi[4:, :]
+        q_i = Yi[:4, :]
 
+        x_cap_k = np.zeros((7, 1))
+        x_cap_k[4:] = np.expand_dims(np.sum(omega_i, axis=1), axis=1) / 12
+
+        q_t = xk_1[:4]
+        e_i = np.zeros((4, 12))
+        for t in range(5):
+            for i in range(12):
+                e_i[:, i] = self.quaternion_multiplication(q_i[:, i], self.quaternion_inverse(q_t)).squeeze()
+            e_i_ = self.convert4dto3d(e_i)
+            e_vect = np.sum(e_i_, axis=1) / 12
+            e = self.convert3dto4done(e_vect)
+            q_t = self.quaternion_multiplication(e, q_t)
+
+        x_cap_k[:4] = q_t
+        return x_cap_k, e_i_
+
+    def covariance_Yi(self, xk_, Yi, rw):
+        xk__repeat = np.repeat(np.expand_dims(xk_, axis=1), 12, axis=1)
+        Wi7d = Yi - xk__repeat
+        Wi6d = np.vstack((self.convert4dto3d(Wi7d[0:4, :]), Wi7d[4:, ]))
+        Wi6d[:3, :] = rw
+        Pk_ = Wi6d.dot(np.transpose(Wi6d))/12
+        return Pk_, Wi6d
+
+    def getMeasurements(self, Yi, sensor = "gyro"):
+
+        if sensor == "gyro":
+            Z_i = Yi[4:, :]
+        else:
+            return
+
+        zk_ = np.sum(Z_i, axis=1)/12
+        innovation = Z_i - np.expand_dims(zk_, axis=1)
+
+        return zk_, Z_i, innovation
+
+    def getMeasurementCovariance(self, zk_, Zi, R):
+        Wz = Zi - np.expand_dims(zk_, axis=1)
+        Pzz = np.dot(Wz, np.transpose(Wz))
+        Pvv = Pzz + R
+        return Pvv
+
+    def CrossCorrelation(self, Wi_, Zi, zk_):
+        return np.dot(Wi_, np.transpose(Zi-np.expand_dims(zk_,axis=1)))
+
+    def KalmanGain(self, Pxz, Pvv, xk_, vk, Pk_):
+        Kk = np.dot(Pxz, np.linalg.inv(Pvv))
+        x_cap = xk_ + np.dot(Kk, vk)
+        Pk = Pk_ - np.dot(Kk, Pvv, np.transpose(Kk))
+
+
+
+
+
+
+
+
+    
 
 
